90acc60687f822979f5c7feded0e5bde9ee9e866
fred.fu <fjw08199999@gmail.com>
2021-11-26 11:18:21 +0800

    Initial Commit

---
+++ MovieSearchAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieSearchAPP
+//
+//  Created by fred fu on 2021/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
